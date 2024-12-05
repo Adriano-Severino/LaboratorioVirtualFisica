03cc75bb9ed405dd82394c1bb00ce676404fd885
Adriano-Severino <adriano.x1@hotmail.com>
2024-12-05 10:37:31 -0300

    Aplicando Solid e Clean Code

--- Program.cs
+++ Program.cs
@@ -4,77 +4,129 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Digite a força aplicada (em Newtons): ");
-        double forca = Convert.ToDouble(Console.ReadLine());
+        var simulador = new SimuladorMovimento(new ConsoleIO());
+        simulador.ExecutarSimulacao();
+    }
+}
 
-        Console.Write("Digite a massa da partícula (em kg): ");
-        double massa = Convert.ToDouble(Console.ReadLine());
+public class SimuladorMovimento
+{
+    private readonly IIO _io;
+    private readonly CalculadorMovimento _calculador;
 
-        double constanteAtrito = 0.1; // constante de atrito
-        bool considerarAtrito = true; // modificar para true ou false conforme necessário
-        double aceleracao = forca / massa; // aceleracao = F / m
+    public SimuladorMovimento(IIO io)
+    {
+        _io = io;
+        _calculador = new CalculadorMovimento();
+    }
 
-        if (considerarAtrito)
+    public void ExecutarSimulacao()
+    {
+        var parametros = ObterParametrosEntrada();
+        MostrarConfiguracaoSimulacao(parametros);
+        ExecutarEMostrarResultados(parametros);
+    }
+
+    private ParametrosSimulacao ObterParametrosEntrada()
+    {
+        double forca = _io.LerDouble("Digite a força aplicada (em Newtons): ");
+        double massa = _io.LerDouble("Digite a massa da partícula (em kg): ");
+        return new ParametrosSimulacao(forca, massa);
+    }
+
+    private void MostrarConfiguracaoSimulacao(ParametrosSimulacao parametros)
+    {
+        _io.Escrever(parametros.ConsiderarAtrito ? "Com atrito" : "Sem atrito");
+        _io.Escrever($"Força aplicada de: {parametros.Forca} Newtons");
+        _io.Escrever($"Massa da particula de: {parametros.Massa} Kgs");
+        _io.Escrever("============================================================");
+        _io.Escrever("Tempo (s)\tPosição (m)\tVelocidade Instantânea (m/s)");
+    }
+
+    private void ExecutarEMostrarResultados(ParametrosSimulacao parametros)
+    {
+        double posicaoFinal = 0;
+        int[] tempos = { 1, 5, 10, 15, 20 };
+
+        foreach (int tempo in tempos)
         {
-            Console.WriteLine("Com atrito");
+            var resultado = _calculador.CalcularMovimento(tempo, parametros);
+            MostrarResultado(tempo, resultado);
+            posicaoFinal = resultado.Posicao;
         }
-        else
-        {
-            Console.WriteLine("Sem atrito");
-        }
-        Console.WriteLine($"Força aplicada de: {forca} Newtons");
-        Console.WriteLine($"Massa da particula de: {massa} Kgs");
-        Console.WriteLine("============================================================");
-        Console.WriteLine("Tempo (s)\tPosição (m)\tVelocidade Instantânea (m/s)");
 
-        double posicaoInicial = 0;
-        double velocidadeInicial = 0;
-
-        // Mostrar os valores em tempos específicos
-        double posicaoFinal = MostrarValores(1, aceleracao, constanteAtrito, considerarAtrito, massa, posicaoInicial, velocidadeInicial);
-        posicaoFinal = MostrarValores(5, aceleracao, constanteAtrito, considerarAtrito, massa, posicaoInicial, velocidadeInicial);
-        posicaoFinal = MostrarValores(10, aceleracao, constanteAtrito, considerarAtrito, massa, posicaoInicial, velocidadeInicial);
-        posicaoFinal = MostrarValores(15, aceleracao, constanteAtrito, considerarAtrito, massa, posicaoInicial, velocidadeInicial);
-        posicaoFinal = MostrarValores(20, aceleracao, constanteAtrito, considerarAtrito, massa, posicaoInicial, velocidadeInicial);
-
-        // Calcular a velocidade média total
         double velocidadeMediaTotal = posicaoFinal / 20;
-
-        Console.WriteLine($"\nVelocidade Média Total: {velocidadeMediaTotal:F2} m/s");
+        _io.Escrever($"\nVelocidade Média Total: {velocidadeMediaTotal:F2} m/s");
     }
 
-    static double MostrarValores(double tempo, double aceleracao, double constanteAtrito, bool considerarAtrito, double massa, double posicaoInicial, double velocidadeInicial)
+    private void MostrarResultado(int tempo, ResultadoMovimento resultado)
     {
-        double posicao, velocidade;
+        _io.Escrever($"Tempo: {tempo}s - Posição: {(int)resultado.Posicao}m - Velocidade Instantânea: {resultado.Velocidade:F2}m/s");
+    }
+}
 
-        if (considerarAtrito)
-        {
-            (posicao, velocidade) = CalcularMovimentoComAtrito(tempo, aceleracao, constanteAtrito, massa, posicaoInicial, velocidadeInicial);
-        }
-        else
-        {
-            posicao = posicaoInicial + velocidadeInicial * tempo + 0.5 * aceleracao * Math.Pow(tempo, 2);
-            velocidade = velocidadeInicial + aceleracao * tempo;
-        }
+public class CalculadorMovimento
+{
+    private const double IntervaloTempo = 0.01;
+    private const double Gravidade = 9.8;
 
-        Console.WriteLine($"Tempo: {tempo}s - Posição: {(int)posicao}m - Velocidade Instantânea: {velocidade:F2}m/s");
-        return posicao;
+    public ResultadoMovimento CalcularMovimento(double tempo, ParametrosSimulacao parametros)
+    {
+        if (parametros.ConsiderarAtrito)
+        {
+            return CalcularMovimentoComAtrito(tempo, parametros);
+        }
+        return CalcularMovimentoSemAtrito(tempo, parametros);
     }
 
-    static (double posicao, double velocidade) CalcularMovimentoComAtrito(double tempo, double aceleracao, double constanteAtrito, double massa, double posicaoInicial, double velocidadeInicial)
+    private ResultadoMovimento CalcularMovimentoSemAtrito(double tempo, ParametrosSimulacao parametros)
     {
-        double intervaloTempo = 0.01;
-        double posicao = posicaoInicial;
-        double velocidade = velocidadeInicial;
+        double aceleracao = parametros.Forca / parametros.Massa;
+        double posicao = 0.5 * aceleracao * Math.Pow(tempo, 2);
+        double velocidade = aceleracao * tempo;
+        return new ResultadoMovimento(posicao, velocidade);
+    }
 
-        for (double t = 0; t < tempo; t += intervaloTempo)
+    private ResultadoMovimento CalcularMovimentoComAtrito(double tempo, ParametrosSimulacao parametros)
+    {
+        double posicao = 0;
+        double velocidade = 0;
+
+        for (double t = 0; t < tempo; t += IntervaloTempo)
         {
-            double forcaAtrito = constanteAtrito * massa * 9.8 * Math.Sign(velocidade);
-            double aceleracaoTotal = (massa * aceleracao - forcaAtrito) / massa;
-            velocidade += aceleracaoTotal * intervaloTempo;
-            posicao += velocidade * intervaloTempo;
+            double forcaAtrito = parametros.ConstanteAtrito * parametros.Massa * Gravidade * Math.Sign(velocidade);
+            double aceleracaoTotal = (parametros.Forca - forcaAtrito) / parametros.Massa;
+            velocidade += aceleracaoTotal * IntervaloTempo;
+            posicao += velocidade * IntervaloTempo;
         }
 
-        return (posicao, velocidade);
+        return new ResultadoMovimento(posicao, velocidade);
+    }
+}
+
+public record ParametrosSimulacao(double Forca, double Massa, double ConstanteAtrito = 0.1, bool ConsiderarAtrito = true)
+{
+    public double Aceleracao => Forca / Massa;
+}
+
+public record ResultadoMovimento(double Posicao, double Velocidade);
+
+public interface IIO
+{
+    double LerDouble(string mensagem);
+    void Escrever(string mensagem);
+}
+
+public class ConsoleIO : IIO
+{
+    public double LerDouble(string mensagem)
+    {
+        Console.Write(mensagem);
+        return Convert.ToDouble(Console.ReadLine());
+    }
+
+    public void Escrever(string mensagem)
+    {
+        Console.WriteLine(mensagem);
     }
 }
